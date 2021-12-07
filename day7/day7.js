6665e6518c579db26d816dae9fee1c7969adf66d
vithusanK <vithusankuperan@gmail.com>
2021-12-07 08:43:47 -0500

    Update day7.js

--- day7/day7.js
+++ day7/day7.js
@@ -3,12 +3,12 @@ const { filePath } = require("../config.json");
 
 // getDayInput(7);
 
-// let input = require("fs")
-//   .readFileSync(`${filePath}/day7/day7.txt`, { encoding: "utf-8" })
-//   .split(",")
-//   .map(Number);
+let input = require("fs")
+  .readFileSync(`${filePath}/day7/day7.txt`, { encoding: "utf-8" })
+  .split(",")
+  .map(Number);
 
-let input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]; //test data
+// let input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]; //test data
 
 function part1() {
   var nums = [];
@@ -19,27 +19,25 @@ function part1() {
   nums.forEach((x) => {
     var currentCost = 0;
     for (let i = 0; i < input.length; i++) {
-        currentCost += Math.abs(input[i] - x);
+      currentCost += Math.abs(input[i] - x);
     }
-    lowest = Math.min(lowest,currentCost);
+    lowest = Math.min(lowest, currentCost);
   });
   console.log("Part 1:" + lowest);
 }
 
 function part2() {
-  var nums = [];
-  input.forEach((crab) => {
-    if (!nums.includes(crab)) nums.push(crab);
-  });
+  var max = Math.max(...input);
+  var min = Math.min(...input);
+
   let lowest = Number.MAX_VALUE;
-  nums.forEach((x) => {
+  for (let j = min; j < max; j++) {
     var currentCost = 0;
     for (let i = 0; i < input.length; i++) {
-      currentCost += getCost(input[i], x);
+      currentCost += getCost(input[i], j);
     }
-    console.log(currentCost);
-    lowest = Math.min(lowest,currentCost);
-  });
+    lowest = Math.min(lowest, currentCost);
+  }
   console.log("Part2: " + lowest);
 }
 
